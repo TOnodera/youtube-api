d3729e471c307794b667484c160da3fc467346f3
t.onodera <t.onodera.1.2.1.5@gmail.com>
2023-03-15 14:03:14 +0000

    dotenvで環境変数読み込めるようになった

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,16 +1,26 @@
 use actix_files::{self as fs, NamedFile};
-use actix_web::{HttpRequest, Result};
+use actix_web::{web::Redirect, Error, HttpRequest, Responder, Result};
 
 use std::path::PathBuf;
 
-async fn index(req: HttpRequest) -> Result<NamedFile> {
+mod env;
+mod types;
+
+async fn index(_: HttpRequest) -> Result<NamedFile> {
     let path: PathBuf = "./frontend/build/index.html".parse()?;
     Ok(NamedFile::open(path)?)
 }
 
-async fn return_url(req: HttpRequest) -> Result<NamedFile> {
-    let path: PathBuf = "./dist/return.html".parse()?;
-    Ok(NamedFile::open(path)?)
+async fn oauth_authorization(_: HttpRequest) -> Result<Redirect> {
+    let redirect_uri = env::get_env("YOUTUBE_API_REDIRECT_URI");
+    let url = match redirect_uri {
+        Ok(url) => url,
+        Err(e) => {
+            return Err(e.into());
+        }
+    };
+
+    Ok(Redirect::to(url).permanent())
 }
 
 #[actix_web::main]
@@ -20,7 +30,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
-            .route("/return", web::get().to(return_url))
+            .route("/oauth", web::get().to(oauth_authorization))
             .service(fs::Files::new("/", "./frontend/build").show_files_listing())
     })
     .bind(("127.0.0.1", 8080))?
