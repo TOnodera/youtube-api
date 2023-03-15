aac17e47cf79e75a71c0a141523b379b1ec400db
t.onodera <t.onodera.1.2.1.5@gmail.com>
2023-03-15 11:48:18 +0000

    OAuth認証でリターンURLを使用するときに問題ないかの確認完了

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::NamedFile;
+use actix_web::{HttpRequest, Result};
+
+use std::path::PathBuf;
+
+async fn index(req: HttpRequest) -> Result<NamedFile> {
+    let path: PathBuf = "./dist/index.html".parse()?;
+    Ok(NamedFile::open(path)?)
+}
+
+async fn return_url(req: HttpRequest) -> Result<NamedFile> {
+    let path: PathBuf = "./dist/return.html".parse()?;
+    Ok(NamedFile::open(path)?)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    use actix_web::{web, App, HttpServer};
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/return", web::get().to(return_url))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
