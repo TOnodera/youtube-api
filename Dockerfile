e39c26c34c516972df29f75d38b6fcd8e6ea3cd6
t.onodera <t.onodera.1.2.1.5@gmail.com>
2023-03-16 14:59:26 +0000

    ローカル実行時のSSL対応実装

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM rust:1.68.0-buster
 
 RUN useradd -m -u 1000 -s /bin/bash rust
-RUN apt-get update && apt-get install -y vim \
+RUN apt-get update && apt-get install -y vim pkg-config libssl-dev \
     && rustup component add rustfmt
 
 # install node
 RUN curl -sL https://deb.nodesource.com/setup_16.x | bash - \
     && apt-get install -y nodejs
+
 
 USER rust
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,5 +1,6 @@
 use actix_files::{self as fs, NamedFile};
-use actix_web::{web::Redirect, Error, HttpRequest, Responder, Result};
+use actix_web::{web::Redirect, HttpRequest, Result};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 use std::path::PathBuf;
 
@@ -27,13 +28,17 @@ async fn oauth_authorization(_: HttpRequest) -> Result<Redirect> {
 async fn main() -> std::io::Result<()> {
     use actix_web::{web, App, HttpServer};
 
+    let mut ssl_builder = SslAcceptor::mozilla_intermediate(SslMethod::tls())?;
+    ssl_builder.set_private_key_file("/workspaces/app/app/pem/key.pem", SslFiletype::PEM)?;
+    ssl_builder.set_certificate_chain_file("/workspaces/app/app/pem/cert.pem")?;
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
             .route("/oauth", web::get().to(oauth_authorization))
             .service(fs::Files::new("/", "./frontend/build").show_files_listing())
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind_openssl(("127.0.0.1", 8080), ssl_builder)?
     .run()
     .await
 }
